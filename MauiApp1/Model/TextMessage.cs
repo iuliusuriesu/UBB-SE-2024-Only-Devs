4f8d07b3e7f783fba111e2c705fae51061909631
tripondavid <davidtripon3@gmail.com>
2024-04-10 14:55:16 +0300

    Implemented messages.

---
+++ MauiApp1/Model/TextMessage.cs
@@ -0,0 +1,19 @@
+﻿using MauiApp1.Model;
+
+namespace Lab3
+{
+    public class TextMessage : Message
+    {
+        private readonly string text;
+
+        public TextMessage(int messageId, int chatId, int senderId, DateTime timestamp, string status, string text) : base(messageId, chatId, senderId, timestamp, status)
+        {
+            this.text = text;
+        }
+
+        public override string GetMessage()
+        {
+            return text;
+        }
+    }
+}
---
+++ MauiApp1/Model/VideoMessage.cs
@@ -0,0 +1,19 @@
+﻿using MauiApp1.Model;
+
+namespace Lab3
+{
+    public class VideoMessage : Message
+    {
+        private readonly string videoPath;
+
+        public VideoMessage(int messageId, int chatId, int senderId, DateTime timestamp, string status, string videoPath) : base(messageId, chatId, senderId, timestamp, status)
+        {
+            this.videoPath = videoPath;
+        }
+
+        public override string GetMessage()
+        {
+            return videoPath;
+        }
+    }
+}
